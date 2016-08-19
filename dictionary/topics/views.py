b288bed8e3c43a9331f50ca81c4a1c592f7f1931
Murvet <murvetkeskin.mk@gmail.com>
2016-08-19 14:03:12 +0300

    refs#1:base template

--- dictionary/topics/views.py
+++ dictionary/topics/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
-# Create your views here.
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.views.generic import DetailView, ListView, RedirectView, UpdateView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render
+from django.contrib import messages
+from .models import User
+
+def profile_detail(request):
+    user = request.user
+    context = {
+        'user' : user,
+    }
+    return render(request, "users/profile.html", context)
+
